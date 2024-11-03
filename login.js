1290c9591b0b096c9c1b9e675d9de7c12ad596c6
Shreyanshpandeyweb <154891736+Shreyanshpandeyweb@users.noreply.github.com>
2024-11-03 11:52:01 +0530

    Update login.js

--- login.js
+++ login.js
@@ -163,8 +163,8 @@ app.post('/signup', async (req, res) => {
   }
 });
 passport.use(new GoogleStrategy({
-  clientID:"569931007438-hhg2j74jno8phqdmg815lco0ckfsvoem.apps.googleusercontent.com",
-  clientSecret: "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",
+  clientID:process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
   callbackURL: "http://localhost:3000/auth/google/secrets",
 },
 async (accessToken, refreshToken, profile, done) => {
