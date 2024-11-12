47150f8cd040720b098f61482a254911a84d1195
vivek-singh-18 <163144658+vivek-singh-18@users.noreply.github.com>
2024-11-13 05:18:19 +0530

    Update main.js

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -5,7 +5,7 @@ function toggleProfileDropdown() {
     dropdown.style.display = dropdown.style.display === 'block' ? 'none' : 'block';
 }
 
-// Close dropdown if clicking outside of profile
+
 document.addEventListener('click', function(event) {
     const profile = document.querySelector('.profilev-v');
     const dropdown = document.getElementById('profileDropdown-v');
@@ -13,6 +13,37 @@ document.addEventListener('click', function(event) {
         dropdown.style.display = 'none';
     }
 });
+
+function filterCourses(category) {
+    const courses = document.querySelectorAll('.course-card-v');
+
+    courses.forEach(course => {
+        const courseCategory = course.getAttribute('data-category');
+
+        if (category === 'all' || courseCategory === category) {
+            course.style.display = 'block'; 
+        } else {
+            course.style.display = 'none'; 
+        }
+    });
+}
+function searchCourses() {
+    const searchInput = document.getElementById('courseSearchInput').value.toLowerCase();
+    const courseCards = document.querySelectorAll('.course-card-v');
+
+    courseCards.forEach(card => {
+        const title = card.getAttribute('data-title');
+        const instructor = card.getAttribute('data-instructor');
+        
+        if (title.includes(searchInput) || instructor.includes(searchInput)) {
+            card.style.display = ''; 
+        } else {
+            card.style.display = 'none'; 
+        }
+    });
+}
+
+
 
 
 
